3f41ab5ff74d1def73f9f11a7e674085bf294bb5
Braydon <burrow.braydon14@gmail.com>
2020-03-01 14:10:12 -0700

    Assignment 58

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,25 @@
+window.alert("Hello,World!");
+
+var A = "This is a string,<br><br> ";
+var A = A.fontcolor("red");
+
+var B = "\"This is a lot harder than it looks don\'t under estimate it.\""
++" -Braydon <br><br>"
+var B = B.fontcolor("Blue");
+
+var family = "Burrows " , Mom = "Holly " , Wife = "Aleisa " , Brother = "Braxton " , Sister = "Breannyn "
+var family = family.fontcolor("green");
+
+document.write(A);
+
+document.write("Lisa told Bart, \"Knock it off, Bart! or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.<br><br> ");
+
+document.write("\"Be who you are and say what you feel,"
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++" -Dr. Seuss <br><br>");
+
+document.write(B);
+
+document.write(family);
+
+document.write(3 + 3);
